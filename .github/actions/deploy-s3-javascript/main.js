e7aabf224ef4007505932cc78622747673023ab6
Victor Delaplaine <victor.delaplaine@hpe.com>
2024-02-16 19:02:33 -0800

    added output for javascript custom action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -7,18 +7,22 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run(){
-    // 1. get some input values
-    const bucket = core.getInput('bucket', {required: true})
-    const bucketRegion = core.getInput('bucket-region', {required: true})
-    const distFolder = core.getInput('dist-folder', {required: true})
-    // 2. upload files - exec package
-    // aws cli command (assuming its installed)
-    exec.exec(`aws s3 sync ${distFolder} s3://${bucket} --region ${bucketRegion}`)
-
-
 
     // log message to gh workflow
-    core.notice('Hello from my custom Javascript Action!')
+    core.notice('Hello from my custom Javascript Action!');
+    // 1. get some input values
+    const bucket = core.getInput('bucket', {required: true});
+    const bucketRegion = core.getInput('bucket-region', {required: true});
+    const distFolder = core.getInput('dist-folder', {required: true});
+    // 2. upload files - exec package
+    // aws cli command (assuming its installed)
+    exec.exec(`aws s3 sync ${distFolder} s3://${bucket} --region ${bucketRegion}`);
+    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+    core.setOutput('website-url', websiteUrl); // echo "website-url=${{ websiteUrl }}
+
+
+
+
 }
 
 
