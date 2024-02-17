52b6fa8dcaa2ee776fdf61d381884ef5a8fd9edc
Victor Delaplaine <victor.delaplaine@hpe.com>
2024-02-16 18:52:21 -0800

    added custom deploy action with javascript/nodejs

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,9 +1,22 @@
 // import gh action packages
 const core = require('@actions/core');
+// gives access to context obj
+// gives access to octect obj - rest api
+//github.getOctokit()
 const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run(){
+    // 1. get some input values
+    const bucket = core.getInput('bucket', {required: true})
+    const bucketRegion = core.getInput('bucket-region', {required: true})
+    const distFolder = core.getInput('dist-folder', {required: true})
+    // 2. upload files - exec package
+    // aws cli command (assuming its installed)
+    exec.exec(`aws s3 sync ${distFolder} s3://${bucket} --region ${bucketRegion}`)
+
+
+
     // log message to gh workflow
     core.notice('Hello from my custom Javascript Action!')
 }
